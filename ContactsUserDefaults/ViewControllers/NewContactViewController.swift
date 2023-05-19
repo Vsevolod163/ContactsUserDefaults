5541b44ce8a07f95e384016bff5efb674e67d1dd
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-20 00:02:58 +0300

    NewContactVC added

--- ContactsUserDefaults/ViewControllers/NewContactViewController.swift
+++ ContactsUserDefaults/ViewControllers/NewContactViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 final class NewContactViewController: UIViewController {
 
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    
+    weak var delegate: NewContactViewControllerDelegate!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let action = UIAction { [weak self] _ in
+            guard let firstName = self?.firstNameTextField.text else { return }
+            self?.doneButton.isEnabled = !firstName.isEmpty
+        }
+        firstNameTextField.addAction(action, for: .editingChanged)
     }
-
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        save()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    private func save() {
+        guard let firstName = firstNameTextField.text else { return }
+        guard let lastName = lastNameTextField.text else { return }
+        
+        let contact = Contact(firstName: firstName, lastName: lastName)
+        
+        delegate.add(contact: contact)
+        dismiss(animated: true)
+    }
 }
