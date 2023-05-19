102050594fc8e2a70d6aa70ba7b35b5e159641ed
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-20 00:02:48 +0300

    ContactsVC code added

--- ContactsUserDefaults/ViewControllers/ContactsViewController.swift
+++ ContactsUserDefaults/ViewControllers/ContactsViewController.swift
@@ -7,6 +7,55 @@
 
 import UIKit
 
+protocol NewContactViewControllerDelegate: AnyObject {
+    func add(contact: Contact)
+}
+
 final class ContactsViewController: UITableViewController {
     
+    private var contacts: [Contact] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let newContactVC = segue.destination as? NewContactViewController else { return }
+        newContactVC.delegate = self
+    }
+}
+
+// MARK: - UITAbleViewDataSource
+extension ContactsViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath)
+        let contact = contacts[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = contact.fullName
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ContactsViewController {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
+
+// MARK: - NewContactViewControllerDelegate
+extension ContactsViewController: NewContactViewControllerDelegate {
+    func add(contact: Contact) {
+        contacts.append(contact)
+        tableView.reloadData()
+    }
 }
