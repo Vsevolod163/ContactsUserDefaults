9b3e18fdc6aa991b87576ca8207d5b9b137fed08
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-20 14:22:37 +0300

    StorageManager added

--- ContactsUserDefaults/Services/StorageManager.swift
+++ ContactsUserDefaults/Services/StorageManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+final class StorageManager {
+    static var shared = StorageManager()
+    
+    private let userDefaults = UserDefaults.standard
+    private let key = "contacts"
+    
+    private init() {}
+    
+    // CRUD - create, read, update, delete
+    func save(contact: Contact) {
+        var contacts = fetchContacts()
+        contacts.append(contact)
+        guard let data = try? JSONEncoder().encode(contacts) else { return }
+        userDefaults.set(data, forKey: key)
+    }
+    
+    func fetchContacts() -> [Contact] {
+        guard let data = userDefaults.data(forKey: key) else { return [] }
+        guard let contacts = try? JSONDecoder().decode([Contact].self, from: data) else { return [] }
+        return contacts
+    }
+    
+    func deleteContact(at index: Int) {
+        var contacts = fetchContacts()
+        contacts.remove(at: index)
+        guard let data = try? JSONEncoder().encode(contacts) else { return }
+        userDefaults.set(data, forKey: key)
+    }
+}
